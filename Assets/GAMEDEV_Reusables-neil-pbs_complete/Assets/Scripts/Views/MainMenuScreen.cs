168c34292890d40e58512e3e50430411f700c89f
Cytoplazam <58141330+Cytoplazam@users.noreply.github.com>
2022-06-01 00:54:08 +0800

    initial robot design

---
+++ Assets/GAMEDEV_Reusables-neil-pbs_complete/Assets/Scripts/Views/MainMenuScreen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScreen : View {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnUIDemoClicked() {
+		LoadManager.Instance.LoadScene (SceneNames.UI_DEMO_SCENE);
+	}
+
+	public void OnObjectPoolClicked() {
+		LoadManager.Instance.LoadScene (SceneNames.OBJECT_POOL_SCENE);
+	}
+
+	public void OnBroadcastingDemoClicked() {
+		LoadManager.Instance.LoadScene (SceneNames.BROADCASTING_SCENE);
+	}
+
+	public void OnSceneLightingClicked() {
+		//LoadManager.Instance.LoadScene (SceneNames.SCENE_LIGHTING);
+        LoadManager.Instance.LoadScene(SceneNames.VISUAL_EFFECTS_MENU_SCENE);
+    }
+
+	public void OnAnimationSceneClicked() {
+		LoadManager.Instance.LoadScene (SceneNames.ANIMATION_DEMO);
+	}
+
+    public void OnAdvancedAnimClicked()
+    {
+        LoadManager.Instance.LoadScene(SceneNames.ADVANCED_ANIMATION_SCENE);
+    }
+
+	public void OnParticleSceneClicked() {
+		LoadManager.Instance.LoadScene (SceneNames.PARTICLE_EFFECT_DEMO);
+	}
+
+    public void OnPathfindingClicked() {
+        LoadManager.Instance.LoadScene(SceneNames.PATHFINDING_SCENE);
+    }
+}
